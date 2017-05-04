d762b0a7f70fde719febfd1c13956e05a3adca3f
Dragon Tran <dragon.tran@ou.edu>
2017-05-04 14:46:32 -0500

    Updates coordinates

--- src/turtlebot/navigation/map_navigation/scripts/control.py
+++ src/turtlebot/navigation/map_navigation/scripts/control.py
@@ -18,10 +18,10 @@ class control():
         self.plannedHighlights = []
 
         self.highlights = []
-        self.highlights.append(highlight("A", 4, 2))
-        self.highlights.append(highlight("B", 1.7, 7))
-        self.highlights.append(highlight("C", 9, 1.4))
-        self.highlights.append(highlight("D", 1, 9))
+        self.highlights.append(highlight("A", 2.25,-1.8))
+        self.highlights.append(highlight("B", 2.8,-0.04))
+        self.highlights.append(highlight("C", 0.7,-1.3))
+        self.highlights.append(highlight("D", 0.86,0.17))
 
         rospy.init_node('control', anonymous=False)
         self.pub = rospy.Publisher('goals', goal, queue_size=10)
